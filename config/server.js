9438ea8cd2efa59da7953fa5a750b07b849c0e43
TusharSinghTawar <166288505+TusharSinghTawar@users.noreply.github.com>
2024-05-10 21:49:54 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+   url:'https://backend-eight-brown.vercel.app/',
   app: {
     keys: env.array('APP_KEYS'),
   },
